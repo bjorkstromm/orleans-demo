1b79e5076489be72af6e83aa0694828a08154cdd
Martin Björkström <martin@bjorkstrom.dev>
2022-11-22 22:22:36 +0200

    Reentrancy demo

---
+++ src/Reentrancy/AlwaysInterleaveGrain.cs
@@ -0,0 +1,48 @@
+﻿using Orleans.Concurrency;
+
+namespace Reentrancy;
+
+public static class AlwaysInterleaveSample
+{
+    public static async Task Run(IGrainFactory factory)
+    {
+        var grain = factory.GetGrain<IAlwaysInterleaveGrain>(Guid.NewGuid());
+
+        await Task.WhenAll(
+            grain.Fast("1"),
+            grain.Fast("2"));
+
+        await Task.WhenAll(
+            grain.Slow("1 (slow)"),
+            grain.Slow("2 (slow)"));
+
+        await Task.WhenAll(
+            grain.Slow("1 (slow)"),
+            grain.Fast("2"));
+    }
+}
+
+public interface IAlwaysInterleaveGrain : IGrainWithGuidKey
+{
+    [AlwaysInterleave]
+    Task Fast(string str);
+
+    Task Slow(string str);
+}
+
+public class AlwaysInterleaveGrain : Grain, IAlwaysInterleaveGrain
+{
+    public Task Fast(string str) => DoWork(str);
+
+    public Task Slow(string str) => DoWork(str);
+
+    private async Task DoWork(string str)
+    {
+        Console.WriteLine($"{str} - First");
+        await Task.Delay(1000);
+        Console.WriteLine($"{str} - Second");
+        await Task.Delay(1000);
+        Console.WriteLine($"{str} - Third");
+        await Task.Delay(1000);
+    }
+}
---
+++ src/Reentrancy/ReentrantGrain.cs
@@ -0,0 +1,35 @@
+﻿using Orleans.Concurrency;
+
+namespace Reentrancy;
+
+public static class ReentrantSample
+{
+    public static async Task Run(IGrainFactory factory)
+    {
+        var grain = factory.GetGrain<IReentrantGrain>(Guid.NewGuid());
+
+        await Task.WhenAll(
+            grain.DoWork("1"),
+            grain.DoWork("2"),
+            grain.DoWork("3"));
+    }
+}
+
+public interface IReentrantGrain : IGrainWithGuidKey
+{
+    Task DoWork(string str);
+}
+
+[Reentrant]
+public class ReentrantGrain : Grain, IReentrantGrain
+{
+    public async Task DoWork(string str)
+    {
+        Console.WriteLine($"{str} - First");
+        await Task.Delay(1000);
+        Console.WriteLine($"{str} - Second");
+        await Task.Delay(1000);
+        Console.WriteLine($"{str} - Third");
+        await Task.Delay(1000);
+    }
+}
