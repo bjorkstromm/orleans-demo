bbc81e08aea763dcc701871a094ce26a401c390c
Martin Björkström <martin@bjorkstrom.dev>
2023-01-17 20:15:51 +0200

    Remove AAD, we'll use Easy Auth instead

--- src/Booking.Admin/Program.cs
+++ src/Booking.Admin/Program.cs
@@ -1,8 +1,4 @@
 using Azure.Identity;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,40 +29,10 @@ builder.Host.UseOrleansClient(clientBuilder =>
 
 // Add services to the container.
 builder.Services.AddApplicationInsightsTelemetry();
-
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
-builder.Services.AddControllersWithViews()
-    .AddMicrosoftIdentityUI();
-
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy
-    options.FallbackPolicy = options.DefaultPolicy;
-});
-
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor()
-    .AddMicrosoftIdentityConsentHandler();
-
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-});
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
-
-app.UseForwardedHeaders();
-
-// For some reason forwarded headers are not working in Azure Container Aoos
-if (!app.Environment.IsDevelopment())
-{
-    app.Use((context, next) =>
-    {
-        context.Request.Scheme = "https";
-        return next(context);
-    });
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -77,12 +43,8 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
