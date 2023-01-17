febb20cb944f7d9d143c657777ea8c972af6ac62
Martin Björkström <martin@bjorkstrom.dev>
2023-01-18 00:09:10 +0200

    Adds user simulation

--- src/Booking.Silo/Grains/UserSimulatorGrain.cs
+++ src/Booking.Silo/Grains/UserSimulatorGrain.cs
@@ -28,7 +28,7 @@ public class UserSimulatorGrain : Grain, IUserSimulatorGrain, IRemindable, IRese
             TimeSpan.Zero,
             TimeSpan.FromMinutes(1));
 
-        _timer = RegisterTimer(OnTick, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(5));
+        _timer = RegisterTimer(OnTick, null, TimeSpan.FromSeconds(Random.Shared.Next(10)), TimeSpan.FromSeconds(5));
 
         return Task.CompletedTask;
     }
