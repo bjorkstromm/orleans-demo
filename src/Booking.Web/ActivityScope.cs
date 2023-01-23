9a6df3bd40bf35aa8d81cb4d3ac4e5a6e76ef265
Martin Björkström <martin@bjorkstrom.dev>
2023-01-24 00:18:06 +0200

    Create new root activities

---
+++ src/Booking.Web/ActivityScope.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+
+namespace Booking;
+
+public class ActivityScope : IDisposable
+{
+    private readonly Activity? _previous;
+    private readonly ActivitySource _source;
+    private readonly Activity? _current;
+
+    public static ActivityScope Create(string name) => new(name);
+
+    private ActivityScope(string name)
+    {
+        _previous = Activity.Current;
+        Activity.Current = null;
+
+        _source = new ActivitySource("booking-admin");
+        _current = _source.StartActivity(name);
+    }
+
+    public void Dispose()
+    {
+        _current?.Dispose();
+        _source?.Dispose();
+
+        Activity.Current = _previous;
+    }
+}
