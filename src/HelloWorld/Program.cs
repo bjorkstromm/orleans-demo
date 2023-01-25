34ca5fff94630a2d69b0c1f1c1dcd07ac0df3a92
Martin Björkström <martin@bjorkstrom.dev>
2023-01-25 11:08:56 +0200

    Add simple hello world sample

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿// Host configuration
+var host = Host.CreateDefaultBuilder()
+    .UseOrleans(x => x.UseLocalhostClustering())
+    .Build();
+
+var client = host.Services.GetRequiredService<IClusterClient>();
+
+await host.StartAsync();
+
+// Grain calls
+var grain = client.GetGrain<IGreeterGrain>(Guid.NewGuid());
+var response = await grain.SayHello("Martin");
+Console.WriteLine(response);
+
+await host.StopAsync();
+
+// Grain interface and implementation
+public interface IGreeterGrain : IGrainWithGuidKey
+{
+    Task<string> SayHello(string name);
+}
+
+public class GreeterGrain : Grain, IGreeterGrain
+{
+    public Task<string> SayHello(string name) =>
+        Task.FromResult($"Hello {name} from {IdentityString}!");
+}
