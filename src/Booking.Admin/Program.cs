eb48d0d269bc883ce0536cc005eff29ad14b2c61
Martin Björkström <martin@bjorkstrom.dev>
2023-09-29 12:05:01 +0300

    Explicit about authN and authZ in the pipeline

--- src/Booking.Admin/Program.cs
+++ src/Booking.Admin/Program.cs
@@ -135,6 +135,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
